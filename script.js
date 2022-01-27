bf6fadac87e73287acf2978ec1ccb3b4cd2533e4
Nicholas Rebusi <>
2022-01-27 21:13:21 +0000

    Grid drawing function finished

--- script.js
+++ script.js
@@ -1,12 +1,65 @@
 let colorlist = ['gold', 'yellow', 'turquoise', 'red']
+var gridwidth = 20
+var curBlocks = [];
+let types = {
+  O: [
+    ['#E5F72C', '#E5F72C'],
+    ['#E5F72C', '#E5F72C']
+  ],
+  J: [
+    ['#1E68FC', null, null],
+    ['#1E68FC', '#1E68FC', '#1E68FC'],
+    [null, null, null]
+  ],
+  L: [
+    [null, null, '#f43'],
+    ['#f43', '#f20', '#f43'],
+    [null, null, null]
+  ],
+  S: [
+    [null, '#f43', '#f43'],
+    ['#f43', '#f43', null],
+    [null, null, null]
+  ],
+  Z: [
+    ['#f43', '#f43', null],
+    [null, '#f43', '#f43'],
+    [null, null, null]
+  ],
+  T: [
+    [null, '#f43', null],
+    ['#f43', '#f43', '#f43'],
+    [null, null, null]
+  ],
+  I: [
+    [null, null, null, null],
+    ['#f43', '#f43', '#f43', '#f43'],
+    [null, null, null, null],
+    [null, null, null, null],
+  ]
+}
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-    background(255);
+  createCanvas(gridwidth * 10 + 1, gridwidth * 20 + 1);
+  background(255);
+  gridDraw();
 }
 
 function draw() {
   noStroke()
   fill(random(colorlist));
-  ellipse(mouseX, mouseY, 25, 25);
+  gridDraw();
+}
+
+function gridDraw() {
+  for (let i = 0; i < 11; i++) {
+    stroke(100);
+    strokeWeight(1);
+    line(i * gridwidth, 0, i * gridwidth, height);
+  }
+  for (let i = 0; i < 21; i++) {
+    stroke(100);
+    strokeWeight(1);
+    line(0, i * gridwidth, width, i * gridwidth);
+  }
 }
